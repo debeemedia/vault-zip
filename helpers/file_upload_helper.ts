fbd277123ed4b8b900059c65d282de68d32522ac
Deborah Okeke <okekedeborah@gmail.com>
2026-02-24 16:44:59 +0100

    fix: fix "request entity too large" error on attempting to upload a large file. This is due to Adonis body parser limit of 20mb - write test to show that large files under the application max file size limit are successfully uploaded

--- helpers/file_upload_helper.ts
+++ helpers/file_upload_helper.ts
@@ -1,3 +1,7 @@
+import app from '@adonisjs/core/services/app'
+
 export const allowedExtensions = ['zip', 'doc', 'docx', 'pdf']
 
 export const allowedPattern = new RegExp(`\\.(${allowedExtensions.join('|')})$`, 'i')
+
+export const maxFileSizeMB = app.inTest ? 50 : 500 // in "mb"
