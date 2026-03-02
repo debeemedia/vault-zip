63e29098e19df8225bcc79751078898161f499f9
Deborah Okeke <okekedeborah@gmail.com>
2026-03-02 23:23:20 +0100

    refactor: improve  streaming decryption for memory efficiency - replace `readFileSync` with `fs.open` for metadata extraction. - drop `writeFileSync` and switch to `stream.pipeline` for file decryption to keep memory usage low - added AES-GCM authentication error handling and partial file cleanup. - added monitoring logs; NB: changes reduced RAM usage from 516MB to 223MB for 108MB file (scalable to larger files).

--- commands/decrypt.ts
+++ commands/decrypt.ts
@@ -5,6 +5,8 @@ import { resolveLicenceKey } from '../helpers/command_helper.js'
 import path from 'node:path'
 import { deriveAESKeyFromLicenceKey, FileMetadata } from '../helpers/file_upload_helper.js'
 import crypto from 'crypto'
+import fsp from 'node:fs/promises'
+import { pipeline } from 'node:stream/promises'
 
 export default class Decrypt extends BaseCommand {
   static commandName = 'vault-zip:decrypt'
@@ -52,30 +54,43 @@ export default class Decrypt extends BaseCommand {
       return (this.exitCode = 1)
     }
 
+    const outputPath = this.filePath.replace(/\.vault$/, '')
+
     try {
       this.logger.info(`Decrypting "${fileName}"...`)
-
-      const encryptedFileBuffer = fs.readFileSync(this.filePath)
 
       /**
        * Reversal of the encrypted bundle packaging done on the server:
        */
-      // 1. Read the first 4 bytes to get the header size
-      const headerSize = encryptedFileBuffer.readUInt32BE(0)
+      // 1. Open the file and read the first 4 bytes to get the header size.
+      // NB: We use the open and allocate approach instead of readFileSync for memory efficiency
+      const handle = await fsp.open(this.filePath, 'r')
+
+      const headerSizeBuffer = Buffer.alloc(4)
+
+      await handle.read(headerSizeBuffer, 0, 4, 0)
+
+      const headerSize = headerSizeBuffer.readUInt32BE(0)
 
       // Validate the file: by checking the metadata JSON size. JSON bigger than 10KB is suspicious
       if (headerSize < 100 || headerSize > 10000) {
+        await handle.close()
+
         this.logger.error('Invalid vault file: Header size is out of bounds.')
 
         return (this.exitCode = 1)
       }
 
       // 2. Extract the metadata JSON based on that size
-      const metadataRaw = encryptedFileBuffer.subarray(4, 4 + headerSize)
+      const metadataRaw = Buffer.alloc(headerSize)
+
+      await handle.read(metadataRaw, 0, headerSize, 4)
 
       // Validate the file signature: by checking for the opening and closing braces of the metadata JSON.
       // In ASCII, "{" is 123 and "}" is 125
       if (metadataRaw[0] !== 123 || metadataRaw[metadataRaw.length - 1] !== 125) {
+        await handle.close()
+
         this.logger.error('Invalid vault file: Metadata is not a valid JSON object.')
 
         return (this.exitCode = 1)
@@ -83,8 +98,9 @@ export default class Decrypt extends BaseCommand {
 
       const metadata = JSON.parse(metadataRaw.toString()) as FileMetadata
 
-      // 3. Everything after (4 + headerSize) is the encrypted file data
-      const encryptedFileData = encryptedFileBuffer.subarray(4 + headerSize)
+      await handle.close()
+
+      // 3. Everything after (4 + headerSize) is the encrypted file data. The read stream will be created after.
 
       /**
        * Client-side decryption. Reversal of the encryption done on the server:
@@ -118,18 +134,35 @@ export default class Decrypt extends BaseCommand {
 
       fileDecipher.setAuthTag(Buffer.from(metadata.fileAuthTag, 'base64'))
 
-      const decryptedFile = Buffer.concat([
-        fileDecipher.update(encryptedFileData),
-        fileDecipher.final(),
-      ])
+      const readStream = fs.createReadStream(this.filePath, { start: 4 + headerSize })
 
-      const outputPath = this.filePath.replace(/\.vault$/, '')
+      // By using streams instead of writeFileSync, we ensure that the command does not hog too much memory
+      const writeStream = fs.createWriteStream(outputPath)
 
-      fs.writeFileSync(outputPath, decryptedFile)
+      await pipeline(readStream, fileDecipher, writeStream)
+
+      /**
+       * For monitoring RAM usage
+       */
+      /* const stats = process.memoryUsage()
+
+      const rss = Math.round(stats.rss / 1024 / 1024)
+
+      const heap = Math.round(stats.heapUsed / 1024 / 1024)
+
+      this.logger.info('--- MEMORY USAGE ---')
+
+      this.logger.info('RAM Claimed (RSS): ' + rss + ' MB')
+
+      this.logger.info('RAM Active Objects: ' + heap + ' MB') */
 
       this.logger.success(`File has been successfully decrypted to: "${outputPath}"`)
     } catch (error) {
-      console.log(error)
+      // Ensure to clean up any partial file
+      if (fs.existsSync(outputPath)) {
+        fs.unlinkSync(outputPath)
+      }
+
       if (
         error.message?.includes('Unsupported state') ||
         error.message?.includes('unable to authenticate data')
